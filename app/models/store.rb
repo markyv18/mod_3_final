3918458ff895baaa81012bcf8cfbce41aab930bf
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-09-13 09:58:24 -0600

    working in postman and store/store-service to generate desired returned stores

--- app/models/store.rb
+++ app/models/store.rb
@@ -6,10 +6,14 @@ class Store
               :store_type
 
   def initialize(store)
-
+    @ = store[:long_name]
+    @ = store[:city]
+    @ = store[:street_address]
+    @ = store[:state]
+    @ = store[:zip]
   end
 
   def self.find_store(zip)
-    
+
 
 end
--- app/services/store_finder.rb
+++ app/services/store_finder.rb
@@ -1,4 +1,8 @@
 class StoreFinder
 
-
+  def self.find_store(zip)
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,city,phone,distance,storeType&pageSize=2&apiKey=#{ENV["BEST_BUY_KEY"]}")
+    JSON.parse(response.body, symbolize_names: true)
+    byebug
+    puts "hi"
 end
